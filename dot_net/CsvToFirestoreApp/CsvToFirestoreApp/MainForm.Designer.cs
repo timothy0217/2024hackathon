e7bc79cf029303b2cb24af30985b188d49d668de
Churton <churton.liang@solventosoft.com.tw>
2024-09-07 18:24:46 +0800

    開放匯入JSON

--- dot_net/CsvToFirestoreApp/CsvToFirestoreApp/MainForm.Designer.cs
+++ dot_net/CsvToFirestoreApp/CsvToFirestoreApp/MainForm.Designer.cs
@@ -20,55 +20,66 @@
 
         private void InitializeComponent()
         {
-            this.comboBoxCollections = new System.Windows.Forms.ComboBox();
-            this.btnUpload = new System.Windows.Forms.Button();
-            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
-            this.lblSelectCollection = new System.Windows.Forms.Label();
-            this.lblSelectFile = new System.Windows.Forms.Label();
-
-            // ComboBox (DropdownList for Firestore collections)
-            this.comboBoxCollections.FormattingEnabled = true;
-            this.comboBoxCollections.Location = new System.Drawing.Point(30, 50);
-            this.comboBoxCollections.Name = "comboBoxCollections";
-            this.comboBoxCollections.Size = new System.Drawing.Size(250, 24);
-            this.comboBoxCollections.TabIndex = 0;
-
-            // Button to upload CSV and import data
-            this.btnUpload.Location = new System.Drawing.Point(30, 150);
-            this.btnUpload.Name = "btnUpload";
-            this.btnUpload.Size = new System.Drawing.Size(250, 30);
-            this.btnUpload.TabIndex = 1;
-            this.btnUpload.Text = "Upload CSV and Import to Firestore";
-            this.btnUpload.UseVisualStyleBackColor = true;
-            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
-
-            // Label for ComboBox
-            this.lblSelectCollection.AutoSize = true;
-            this.lblSelectCollection.Location = new System.Drawing.Point(30, 30);
-            this.lblSelectCollection.Name = "lblSelectCollection";
-            this.lblSelectCollection.Size = new System.Drawing.Size(115, 17);
-            this.lblSelectCollection.TabIndex = 2;
-            this.lblSelectCollection.Text = "Select Collection";
-
-            // Label for File Upload
-            this.lblSelectFile.AutoSize = true;
-            this.lblSelectFile.Location = new System.Drawing.Point(30, 100);
-            this.lblSelectFile.Name = "lblSelectFile";
-            this.lblSelectFile.Size = new System.Drawing.Size(130, 17);
-            this.lblSelectFile.TabIndex = 3;
-            this.lblSelectFile.Text = "Select CSV File";
-
-            // Form configuration
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(320, 200);
-            this.Controls.Add(this.lblSelectFile);
-            this.Controls.Add(this.lblSelectCollection);
-            this.Controls.Add(this.comboBoxCollections);
-            this.Controls.Add(this.btnUpload);
-            this.Name = "MainForm";
-            this.Text = "Firestore CSV Uploader";
-            this.Load += new System.EventHandler(this.MainForm_Load);
+            comboBoxCollections = new ComboBox();
+            btnUpload = new Button();
+            openFileDialog = new OpenFileDialog();
+            lblSelectCollection = new Label();
+            lblSelectFile = new Label();
+            SuspendLayout();
+            // 
+            // comboBoxCollections
+            // 
+            comboBoxCollections.FormattingEnabled = true;
+            comboBoxCollections.Location = new Point(34, 59);
+            comboBoxCollections.Margin = new Padding(3, 4, 3, 4);
+            comboBoxCollections.Name = "comboBoxCollections";
+            comboBoxCollections.Size = new Size(281, 27);
+            comboBoxCollections.TabIndex = 0;
+            // 
+            // btnUpload
+            // 
+            btnUpload.Location = new Point(34, 178);
+            btnUpload.Margin = new Padding(3, 4, 3, 4);
+            btnUpload.Name = "btnUpload";
+            btnUpload.Size = new Size(314, 36);
+            btnUpload.TabIndex = 1;
+            btnUpload.Text = "Upload CSV,JSON and Import to Firestore";
+            btnUpload.UseVisualStyleBackColor = true;
+            btnUpload.Click += btnUpload_Click;
+            // 
+            // lblSelectCollection
+            // 
+            lblSelectCollection.AutoSize = true;
+            lblSelectCollection.Location = new Point(34, 36);
+            lblSelectCollection.Name = "lblSelectCollection";
+            lblSelectCollection.Size = new Size(123, 19);
+            lblSelectCollection.TabIndex = 2;
+            lblSelectCollection.Text = "Select Collection";
+            // 
+            // lblSelectFile
+            // 
+            lblSelectFile.AutoSize = true;
+            lblSelectFile.Location = new Point(34, 119);
+            lblSelectFile.Name = "lblSelectFile";
+            lblSelectFile.Size = new Size(111, 19);
+            lblSelectFile.TabIndex = 3;
+            lblSelectFile.Text = "Select CSV File";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(360, 238);
+            Controls.Add(lblSelectFile);
+            Controls.Add(lblSelectCollection);
+            Controls.Add(comboBoxCollections);
+            Controls.Add(btnUpload);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "MainForm";
+            Text = "Firestore CSV Uploader";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
     }
 }
